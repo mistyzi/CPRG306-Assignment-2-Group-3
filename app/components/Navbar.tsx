70959061e14fe3e974a6ff9f123101a0f6156328
AgentJay2000 <jfwrideout@gmail.com>
2026-06-25 14:57:12 -0600

    Added Comments

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,11 @@
+/**
+ * Student Name: Misty, Zabdiel, Jonah
+ * Date: June 25, 2026
+ * This component is for the top navbar, it display the heading for Student Dashboard,
+ * and the user profile indicator
+*/
 export default function Navbar() {
+// Renders the top navbar 
   return (
     <header className="w-full bg-white border-b border-gray-200 py-4 px-10 flex items-center justify-between">
       <h2 className="text-lg font-semibold text-gray-900">Student Dashboard</h2>
