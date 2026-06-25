c08bf7ac9f36248ba0efd3dc83775c4eb88dd14c
AgentJay2000 <jfwrideout@gmail.com>
2026-06-25 14:50:13 -0600

    Added Comments
    Added Comments

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,4 +1,12 @@
+/**
+ * Student Name: Misty, Zabdiel, Jonah
+ * Date: June 25, 2026
+ * Program Description: This component serves as the footer for the school portal. 
+ * It display the school copyright notice physical address 
+ */
+
 export default function Footer() {
+// Renders the footer section
   return (
     <footer className="py-6 text-center text-xs text-gray-400 mt-12">
       © 2026 Southern Alberta Institute of Technology
